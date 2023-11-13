9151d2c00f37fccafb6c02b8647e3c2a77aaf742
elisaflemer <elisaflemer@gmail.com>
2023-11-12 23:49:24 -0300

    feat: mapear

---
+++ ws/src/mapear/mapear_e_salvar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Function to run when Ctrl+C is pressed
+save_map() {
+    ros2 run nav2_map_server map_saver_cli -f map
+    # Add your cleanup command here
+    # For example: kill some_process
+
+    # Wait for the background process to finish before exiting
+    wait
+    exit 0
+}
+
+# Set the cleanup function to be called on Ctrl+C
+trap save_map SIGINT
+
+# Your main command goes here
+ros2 launch launch/map_launch.py &
+
+# Wait for the background process to finish
+wait
+
